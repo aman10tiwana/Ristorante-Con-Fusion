5985fe45a4a4866467aa71c759b3f9eec88fe75f
Amninder Kaur <akaur@scu.edu>
2022-05-03 11:55:09 -0400

    SPA part-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
-
+import { Breadcrumb, BreadcrumbItem, Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) {
       return (
-          <div className='col-12 col-md-5 m-1'>
+          <div>
               <Card>
                   <CardImg width="100%" src={dish.image} alt={dish.name} />
                   <CardBody>
@@ -18,7 +18,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
     function RenderComments({comments}){
       return (
-         <div className="col-12 col-md-5 m-1">
+         <div>
             <h4>Comments</h4>
             <ul className="list-unstyled">
             {comments.map((comment) => {
@@ -39,8 +39,22 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
           return (
               <div className="container">
                 <div className="row">
-                   <RenderDish dish={props.dish} />
-                   <RenderComments commments={props.dish.comments} />
+                  <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                  </Breadcrumb>
+                  <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-12 col-md-5 m-1">
+                      <RenderDish dish={props.dish} />
+                  </div>
+                  <div className="col-12 col-md-5 m-1">
+                      <RenderComments comments={props.comments} />
+                  </div>
                 </div>
               </div>
           );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
 import Contact from './ContactComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -8,7 +9,7 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
-import { Route, Routes, Navigate} from 'react-router-dom';
+import { Route, Routes, Navigate, useParams} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -24,7 +25,6 @@ class Main extends Component {
   }
 
   render() {
-
     const HomePage = () => {
       return(
         <Home
@@ -33,20 +33,30 @@ class Main extends Component {
         leader={this.state.leaders.filter((leader) => leader.featured)[0]}
         />
       );
-    }
+    };
 
     const MenuPage = () => {
       return(
         <Menu dishes={this.state.dishes} />
       );
-    }
+    };
+
+    const DishWithId = () => {
+      let { dishId } =useParams();
+      return(
+        <DishDetail dish= {this.state.dishes.filter((dish) => dish.id === parseInt(dishId, 10))[0]}
+         comments= {this.state.comments.filter((comment) => comment.dishId === parseInt(dishId, 10))}
+        />
+      );
+    };
 
     return (
       <div>
         <Header />
         <Routes>
               <Route path="/home" element={<HomePage />} />
-              <Route path="/menu" element={<MenuPage />} />
+              <Route exact path="/menu" element={<MenuPage />} />
+              <Route path='/menu/:dishId' element={<DishWithId/>} />
               <Route path="/contactus" element={<Contact/>} />
               <Route path="/*" element={<Navigate replace to="/home" />} />
         </Routes>
