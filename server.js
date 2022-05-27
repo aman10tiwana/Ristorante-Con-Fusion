98c11374713e7231acccc0dbd4f3df42ff3c8b88
Amninder Kaur <akaur@scu.edu>
2022-05-27 18:25:48 -0400

    first deployment'

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const jsonServer = require('json-server');
 const app = jsonServer.create();
-const path = require('path');
-const express = require('express');
-const middlewares = jsonServer.defaults();
-const router = jsonServer.router('db.json');
-const port = process.env.PORT || 3001;
-
-app.use('/db', middlewares, router);
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: "./build"
 });
-
-server.listen(port);
+const port = process.env.PORT || 3001;
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1"
+  })
+);
+server.use(router);
+server.listen(port, () => {
+console.log("Server is running on ${port}");
+});
